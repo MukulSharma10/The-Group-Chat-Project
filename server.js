e1ef3ea13e853ca575bcbe1c923b64e7fa734023
Mukul Sharma <mukulsharmaudemy@gmail.com>
2025-01-10 12:25:20 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import WebSocket, { WebSocketServer } from 'ws'
+
+const app = express()
+const port = 3000
+
+const server = app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+})
+
+const wss = new WebSocketServer({server})
+
+wss.on('connection', (ws)=>{
+    console.log('New client connected')
+
+    ws.on('message', (message)=>{
+        console.log(`Received: ${message}`)
+
+        //Broadcast the message to all connected clients
+        wss.clients.forEach(client =>{
+            if (client.readyState === WebSocket.OPEN){
+                client.send(message)
+            }
+        })
+    })
+
+    ws.on('close', ()=>{
+        console.log('Client disconnected')
+    })
+})
