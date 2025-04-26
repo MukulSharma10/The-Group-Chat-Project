7ec5904f6af8ff2da9b36c76276cbfda6d77f5fe
Mukul <68728825+MukulSharma10@users.noreply.github.com>
2025-04-26 14:45:34 +0530

    Integrated Postgresql for storing messages

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ import express from 'express'
 import http from 'http'
 import path from 'path'
 import { Server } from 'socket.io'
+import pkg from 'pg'
+const { Pool } = pkg
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+})
 
 let connectedUsers = 0
 const MAX_USERS = 12
@@ -25,9 +38,18 @@ io.on('connection', (socket)=>{
     io.emit('update user count', connectedUsers)
 
     io.emit('message',  `${nickname} has joined the chat`)
-    socket.on('user-message', (message) =>{
+    socket.on('user-message', async(message) =>{
         console.log(`${nickname} wrote: `, message)
         io.emit("message", `${nickname}: ${message}`)
+
+        try{
+            await pool.query(
+                'INSERT INTO messages(sender, content) VALUES($1, $2)',
+                [nickname, message]
+            )
+        } catch(err){
+            console.error('Error saving the message to database:', err);
+        }
     })
 
     socket.on('disconnect', ()=>{
@@ -36,13 +58,17 @@ io.on('connection', (socket)=>{
         io.emit('update user count', connectedUsers)
         io.emit('message', `${nickname} has left the chat`)
     })
+
+    socket.on('typing', () =>{
+        socket.broadcast.emit('typing', nickname)
+    })
 })
 
 //Express handles http requests
 app.use(express.static(path.resolve("./public")))
 
 app.get('/', (req, res)=>{
-    return res.sendFile('/public/index.html')
+    return res.sendFile(path.resolve('/public/index.html'))
 })
 
 server.listen(9000, ()=>{
